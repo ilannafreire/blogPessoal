a17cb9829b11c1bb65038db07e132265d4712fd9
ilannafreire <ilannafreiret@hotmail.com>
2021-10-07 00:04:34 -0300

    sec

---
+++ src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -0,0 +1,5 @@
+package org.generation.blogPessoal.model;
+
+public class Usuario {
+
+}
--- src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
+++ src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
@@ -1,5 +1,7 @@
 package org.generation.blogPessoal.repository;
 
+import java.util.List;
+
 import org.generation.blogPessoal.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,6 +10,8 @@ import org.springframework.stereotype.Repository;
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 	
    //public List<Postagem> findAllbyTituloContainingIgnoreCase (String titulo);
-   
+	
+		
+public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo); 
 	
 }
