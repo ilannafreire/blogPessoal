a4997a5a5afa4a8012f8010831609a6c29412590
ilannafreire <ilannafreiret@hotmail.com>
2021-10-15 15:08:25 -0300

    Junit

--- src/main/java/org/generation/blogPessoal/model/Usuario.java
+++ src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -1,12 +1,20 @@
 package org.generation.blogPessoal.model;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.sun.istack.NotNull;
 
 @Entity
@@ -29,6 +37,25 @@ public class Usuario {
 	@Size (min= 5, max =100)
 	private String senha;
 
+	@OneToMany (mappedBy = "usuario", cascade = CascadeType.REMOVE)
+    @JsonIgnoreProperties("usuario")
+    private List <Postagem> postagem;
+    
+    @Column(name = "dt_nascimento")
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private LocalDate dataNascimento;
+    
+
+	public Usuario(long id, String nome, String usuario, String senha) {
+	  this.id = id;
+	  this.nome = nome;
+	  this.usuario = usuario;
+	  this.senha = senha;
+	 
+	 
+	}
+      
+	public Usuario() {}
 	
 	public long getId() {
 		return id;
--- src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
+++ src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package org.generation.blogPessoal.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.generation.blogPessoal.model.Usuario;
@@ -7,5 +8,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	public Optional<Usuario> findByUsuario(String usuario);
+
+	public List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
+	public Usuario findByNome(String nome);
 
 }
